7e9b8088b70cc165adcf4f34e6a0ff413f28fab5
Peter_Ujvari <Peter_Ujvari@epam.com>
2017-01-10 18:01:21 +0100

    test lil progress

--- test/whalegame.js
+++ test/whalegame.js
@@ -1,5 +1,17 @@
 contract('Whalegame', function(accounts){
-  it("", function(){
-    
+  it("should become the new whale", function(){
+    var wg = Whalegame.deployed();
+    var amount;
+    var step;
+
+    wg.whale.amount.call().then(function(_amount){
+      amount = _amount.toNumber();
+      return wg.stepPercent.call();
+    }).then(function(_step){
+      step = _step.toNumber();
+      return wg.play({from: accounts[0], value: amount + step + 1});
+    }).then(function(){
+
+    });
   });
 });
