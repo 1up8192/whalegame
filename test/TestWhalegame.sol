a53e8bde59367277274bff9aed8908611adeaf20
Peter_Ujvari <Peter_Ujvari@epam.com>
2017-02-23 16:50:29 +0100

    dummy sol test

--- test/TestWhalegame.sol
+++ test/TestWhalegame.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.8;
 
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/MetaCoin.sol";
+
 contract TestWhalegame {
+
+  function testExample() {
+    Assert.equal(true, true, "Works");
+  }
 
 }
